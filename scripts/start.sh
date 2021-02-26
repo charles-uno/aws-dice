a69816a189a60c748170c53a7d8cfaa4dedde993
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2021-02-25 22:00:01 -0600

    no make?

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-make
+
+PORT=5001
+NAME=aws-dice
+
+docker build . -f Dockerfile -t ${NAME}
+
+docker run -p ${PORT}:${PORT} --name ${NAME} --rm ${NAME} \
+		gunicorn app:app -b 0.0.0.0:${PORT}
