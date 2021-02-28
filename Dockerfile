448cffebe561c8c9f82ff9fb4b3d0e257a887810
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2021-02-27 21:57:37 -0600

    docker compose?

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM python:3.7-slim
-
 RUN pip install flask gunicorn
-
 ADD app.py /app.py
--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT=80
-NAME=aws-practice
-
-docker build . -f Dockerfile -t ${NAME}
-
-docker run -d -p ${PORT}:${PORT} --name ${NAME} --rm ${NAME} \
-		gunicorn app:app -b 0.0.0.0:${PORT}
