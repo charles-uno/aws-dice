33ae5312c1808af2f0fe05c89a9d797e13fcedcb
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2021-04-02 13:13:47 -0500

    pre-deploy commit

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,10 +6,15 @@ while :; do
     sleep 120
 done
 
+certbot --help
+
+
+
 certbot \
     --non-interactive \
     --agree-tos \
     -m charles.public.account@gmail.com \
     --nginx \
     -d flashcards.charles.uno \
-    -d www.flashcards.charles.uno
+    -d www.flashcards.charles.uno \
+    --redirect
